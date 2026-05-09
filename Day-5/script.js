44df96e051a668e5b06e7019ca36e91801b8d279
Shlok Punjal <196046680+shlokpunjal@users.noreply.github.com>
2026-05-09 09:28:58 +0530

    Day 5 files updated with correct code

--- Day-5/script.js
+++ Day-5/script.js
@@ -3,7 +3,7 @@ const devData = {
     title: "Web Developer",
     location: "India",
     bio: "Diploma student. Won a comp with pure CSS. Now learning JS and breaking things intentionally.",
-    skills: ["HTML", "CSS", "JS"],
+    skills: [],
     links: {
         github: "https://github.com/shlokpunjal",
         linkedin: "https://www.linkedin.com/in/shlokpunjal/"
@@ -21,22 +21,51 @@ const buildCard = () => {
         </div>
         <div id= "card-body">    
             <p id = "bio">${devData.bio}</p>
-            <p id = "skills">${devData.skills.map(skill => `<span class = "skill">${skill}</span>`).join(" ")}</p>
+            <p id = "skills">
+                ${devData.skills.map((skill,index) => 
+                    `<span data-index="${index}" onclick="toggleSkill(${index})" 
+                        class = "skill ${skill.status === 'mastered' ? 'mastered' : ''}" >
+                    ${skill.name} · ${skill.status} <button class="del-btn" onclick="deleteSkill(${index})">X</button></span>`).join(" ")}
+            </p>
+            <div id = "add-skill">
+                <input id = "skill-inp" type = "text" placeholder="Enter Skill">
+                <button id = "add">+ Add</button>
+            </div>
             <hr>
             <a href = "${devData.links.github}" target="_blank">Github</a>
             <a href = "${devData.links.linkedin}" target="_blank">Linkedin</a>
         </div>
     `
+    
+    const btn = document.querySelector("#add")
+
+    btn.addEventListener('click', function () {
+        addSkill()
+    })
 }
 
 buildCard()
 
-const box = document.querySelector(".container")
-const btn = document.querySelector("#btn")
-const state = document.getElementById("State")
+const addSkill = () => {
+    const inp = document.querySelector("#skill-inp")
+    const skill = inp.value
 
-btn.addEventListener('click', function () {
-    box.classList.toggle("highlighted")
+    devData.skills.push({name: skill, status: "learning"})
 
-    state.innerHTML = box.classList.contains("highlighted") ? "ON" : "OFF"
-})
+    buildCard()
+    inp.value = ""
+}
+
+const toggleSkill = (index) => {
+    if(devData.skills[index].status === "learning"){
+        devData.skills[index].status = "mastered"
+    }else{
+        devData.skills[index].status = "learning"
+    }
+    buildCard()
+}
+
+const deleteSkill = (index) => {
+    devData.skills.splice(index, 1)
+    buildCard()
+}
