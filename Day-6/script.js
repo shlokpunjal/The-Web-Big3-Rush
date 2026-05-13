4b406ac600491fef7b42c8c922b29cf2eb235364
Shlok Punjal <196046680+shlokpunjal@users.noreply.github.com>
2026-05-13 16:33:11 +0530

    Update Day-6 script

--- Day-6/script.js
+++ Day-6/script.js
@@ -2,6 +2,25 @@ const input = document.getElementById('inp-note')
 const addBtn = document.getElementById('add-note')
 const container = document.querySelector('.container')
 
+const notes = JSON.parse(localStorage.getItem('notes')) || []
+
+for (let index = 0; index < notes.length; index++) {
+    const note = document.createElement('div')
+    note.classList.add('note')
+    note.dataset.index = index
+
+    const text = document.createElement('p')
+    text.textContent = notes[index]
+
+    const delBtn = document.createElement('button')
+    delBtn.classList.add('delete')
+    delBtn.textContent = 'X'
+
+    container.appendChild(note)
+    note.appendChild(text)
+    note.appendChild(delBtn)
+}
+
 addBtn.addEventListener('click', () => {
     const note = document.createElement('div')
     note.classList.add('note')
@@ -16,10 +35,17 @@ addBtn.addEventListener('click', () => {
     container.appendChild(note)
     note.appendChild(text)
     note.appendChild(delBtn)
+    notes.push(input.value)
+
+    localStorage.setItem('notes', JSON.stringify(notes))
 })
 
 container.addEventListener('click', () => {
     if(event.target.classList.contains('delete')){
         event.target.parentElement.remove()
     }
+
+    const idx = event.target.parentElement.dataset.index  
+    notes.splice(idx, 1)
+    localStorage.setItem('notes', JSON.stringify(notes))
 })
